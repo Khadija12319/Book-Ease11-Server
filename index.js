8b0e8f77dec7d688c775e98cb8b1585549d210b2
Khadija12319 <khadijaatul15-12319@diu.edu.bd>
2024-05-11 01:21:30 +0600

    password changed successfully

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-//name=BookEase
-//password=VLJISvdpRGrH0225
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy4ghoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
